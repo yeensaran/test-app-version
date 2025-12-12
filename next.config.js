412c7483674e72cb1b756ffdfea0d5dcebe131d7
yeensaran <saran@skilllane.com>
2025-12-12 10:18:50 +0700

    init

---
+++ next.config.js
@@ -0,0 +1,15 @@
+
+
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
