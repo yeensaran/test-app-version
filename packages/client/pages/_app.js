0f82fb19c84649bd98ba2afe2101845c8321945e
yeensaran <saran@skilllane.com>
2025-12-12 14:52:00 +0700

    init

--- packages/client/pages/_app.js
+++ packages/client/pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
     try {
       console.log(' =====>> |Check App Version| <<=====');
 
-      const latestClientVersion = process.env.APP_VERSION || '3.0.1';
+      const latestClientVersion = process.env.APP_VERSION || '3.0.2';
 
       if (!latestClientVersion) {
         console.log('--- No client version found ---');
